862be138fb665f7568a77190d5af1256742b2451
Zachary Mason <zachary@northstar-tutors.com>
2022-04-18 15:01:13 +0200

    boilerplate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'full-stack-test-1-4182022',
+    appId: '1:701312938053:web:05343b1e05d3b7cd27732c',
+    storageBucket: 'full-stack-test-1-4182022.appspot.com',
+    apiKey: 'AIzaSyCpBhhaoGDdKuB2UG4cly1ivx7iv78YZdE',
+    authDomain: 'full-stack-test-1-4182022.firebaseapp.com',
+    messagingSenderId: '701312938053',
+  },
   production: true
 };
